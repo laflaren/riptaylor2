5dac5c6f9b0b00cb428a9ac5a891b31ae5edeea7
Natalie La Flare <69825896+laflaren@users.noreply.github.com>
2023-04-07 11:01:34 -0500

    initial commit from desktop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import './components/Form.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <header>
+      <a className="App-link" href="http://taylormarieh.com/i-was-busy-thinking-about-boys"> Zine </a>
       </header>
+
+        <h1>
+        <span className='c1'> T </span>
+        <span className='c2'> a </span>
+        <span className='c3'> y </span>
+        <span className='c4'> l </span>
+        <span className='c5'> o </span>
+        <span className='c6'> r </span>
+        <span className='c7'> &nbsp; </span>
+        <span className='c8'> H </span>
+        <span className='c9'> e </span>
+        <span className='c10'> a </span>
+        <span className='c11'> g </span>
+        <span className='c12'> l </span>
+        <span className='c13'> e </span>
+        <span className='c14'> r </span>
+        </h1>
+
+        <h2> Artist, Curator, Advocate, Traveler </h2>
+
+        <h3> 1995 - 2022 </h3>
+
+        <footer> 
+          <div className="holder">
+            <div className="candle">
+              <div className="blinking-glow"> </div>
+                <div className="thread"> </div>
+                  <div className="glow"> </div>
+                    <div className="flame"> </div>
+            </div>
+          </div>
+
+          <div> 
+            <a className='memories' href="Form.js"> Share a memory </a>
+          </div>
+
+        </footer>
+
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/components/Form.js
@@ -0,0 +1,9 @@
+function Form(){
+  return(
+    <div> 
+      <h1>Word</h1>
+    </div>
+  )
+}
+
+export default Form;
